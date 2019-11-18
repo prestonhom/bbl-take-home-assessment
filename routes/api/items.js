d4ad17c3bd7e978287adafee0b638ecdc9793e6c
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-11-18 08:48:16 -0800

    deploys

--- routes/api/items.js
+++ routes/api/items.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const itemsCtrl = require('../../controllers/items');
 
 router.get('/', itemsCtrl.indexItems)
-router.post('/add', itemsCtrl.addItem)
+router.post('/items/add', itemsCtrl.addItem)
 
 module.exports = router;
